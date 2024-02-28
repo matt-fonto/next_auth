67f2ea63b3437f14c030b610de8bafcfee241a96
SoftEngMatthew <mateus.fonto@gmail.com>
2024-02-28 21:16:45 +0100

    base working version

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,54 @@
-import Image from "next/image";
+// import { getSession } from "next-auth/react";
 
-export default function Home() {
+import { getSession, login, logout } from "@/lib";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getSession();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>hey</h2>
-    </main>
+    <section className="min-h-screen">
+      <div className="flex flex-col w-1/4 justify-center items-center mx-auto mt-20">
+        {/* Login */}
+        <form
+          className="space-y-4"
+          action={async (formData) => {
+            "use server";
+            await login(formData); // call this function
+            redirect("/"); // redirect to home page
+          }}
+        >
+          <label>Email</label>
+          <input type="email" name="email" required />
+          <br />
+
+          <label>Password</label>
+          <input type="password" name="password" required />
+          <br />
+
+          <label>Name</label>
+          <input type="text" name="name" required />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+
+        {/* Logout */}
+        <form
+          action={async () => {
+            "use server";
+            await logout();
+            redirect("/");
+          }}
+        >
+          <button type="submit">Logout</button>
+        </form>
+
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+    </section>
   );
 }
 
 // https://nextjs.org/learn/dashboard-app/adding-authentication
+// https://www.youtube.com/watch?v=DJvM2lSPn6w&t=535s
+// https://github.com/balazsorban44/auth-poc-next
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import { NextRequest } from "next/server";
+import { updateSession } from "./lib";
 
-export default NextAuth(authConfig).auth;
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-};
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
